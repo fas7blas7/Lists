3a0c4eada488a436b73f063f62ee841d8cef7ff0
Martin Kirov <fas7blas7@gmail.com>
2025-05-28 21:22:52 +0300

    Adding ChangeLists task

---
+++ 2-ChangeLists/Program.cs
@@ -0,0 +1,36 @@
+﻿List<int> numbers = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToList();
+
+string input = Console.ReadLine();
+while (input != "end")
+{
+    string[] cmndArg = input.Split(' ');
+
+    string command = cmndArg[0];
+    if (command == "Delete")
+    {
+        int elementToRemove = int.Parse(cmndArg[1]);
+
+        for(int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] == elementToRemove)
+            {
+                numbers.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+    else if (command == "Insert")
+    {
+        int element = int.Parse(cmndArg[1]);
+        int index = int.Parse(cmndArg[2]);
+
+        numbers.Insert(index, element);
+    }
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine(string.Join(" ", numbers));
