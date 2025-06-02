6dd086e938716071d12ac8f19faeda8c6320f47b
Martin Kirov <fas7blas7@gmail.com>
2025-06-02 22:04:14 +0300

    Adding DemoLists

---
+++ 4-DemoLists/Program.cs
@@ -0,0 +1,80 @@
+﻿////List<int> numbers = new List<int>();
+
+////numbers.Add(0);
+//numbers.Add(1);
+//numbers.Add(2);
+//numbers.Add(3);
+//numbers.Add(4);
+//numbers.Add(5);
+//numbers.Add(6);
+
+////Console.WriteLine(numbers);
+
+
+//List<string> names = new List<string>() { "Anatoli", "Petar", "Gosho" };
+
+//Console.WriteLine(names.Count);
+//Console.WriteLine(string.Join(", ", names));
+
+//if (!names.Remove("Dimitrichko"))
+//{
+//    Console.WriteLine("this name is not in the list");
+//}
+
+//names.Add("Gosho");
+//names.Add("Gosho");
+//names.RemoveAll(n => n == "Gosho");
+
+//names.Insert(2, "Martin");
+//Console.WriteLine(names.Count);
+//Console.WriteLine(string.Join(", ", names));
+
+//if (names.Contains("Martin"))
+//{
+//    Console.WriteLine("YES!");
+//}
+//else Console.WriteLine("NOPE!");
+
+//names.Sort();
+//Console.WriteLine(string.Join(", ", names));
+
+//using System.Globalization;
+
+//List<int> nums = new List<int> { 10, 20, 30, 40 , 50, 60 };
+
+//nums.Remove(30);
+//Console.WriteLine(string.Join(" ", nums));
+
+//nums.Add(100);
+//Console.WriteLine(string.Join(" ", nums));
+
+//nums.Insert(0, -100);
+//Console.WriteLine(string.Join(" ", nums));
+//Console.WriteLine(nums.Count);
+
+//int n = int.Parse(Console.ReadLine());
+//List<int> numbers = new List<int>();
+
+//for (int i = 0; i < n; i++)
+//{
+//    int num = int.Parse(Console.ReadLine());
+//    numbers.Add(num);
+//}
+
+//Console.WriteLine(string.Join(", ", numbers));
+
+//string values = "10 45 30 12 5 7";
+//List<string> items = values.Split(' ').ToList();
+
+//List<int> numbers = Console.ReadLine()
+//    .Split(" ")
+//    .Select(int.Parse)
+//    .ToList();
+
+//for (int i = 0; i < numbers.Count; i++)
+//{
+//    Console.Write(numbers[i] + " ");
+//}
+
+//Console.WriteLine(string.Join(", ", numbers));
+
